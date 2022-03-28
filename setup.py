07baca3fa75e244fa41ff3bc15293fb247ee7855
Juan Gamella Martin <juan.gamella@stat.math.ethz.ch>
2022-03-28 13:47:40 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='utlvce',
+    version='0.0.1',
+    author='Juan L Gamella',
+    author_email='juangamella@gmail.com',
+    packages=['utlvce'],
+    scripts=[],
+    url='https://github.com/juangamella/ut-lvce',
+    license='BSD 3-Clause License',
+    description='',
+    long_description=open('README_pypi.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['numpy>=1.17.0']
+)
